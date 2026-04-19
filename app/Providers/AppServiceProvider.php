493b339429ef98b828359b872e03a2dab67529df
trithanh123 <phamtrithanh1234@gmail.com>
2026-04-20 02:20:17 +0700

    Cau hinh lai cho bien moi truong cho render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Ép toàn bộ link (kể cả CSS/JS) sang HTTPS khi đưa lên mạng
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
